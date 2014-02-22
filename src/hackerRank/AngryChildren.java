749581dbb2cb9a80b074b6a8f3dcee2c4e0e6696
Victor Regalado <vrrs0988@gmail.com>
2014-02-22 00:10:23 -0500

    Finished my O(nlogn) version of AngryChildren problem

--- src/hackerRank/AngryChildren.java
+++ src/hackerRank/AngryChildren.java
@@ -1,11 +1,6 @@
 package hackerRank;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Deque;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 
 /*
@@ -23,90 +18,25 @@ import java.util.Scanner;
 */
 public class AngryChildren {
 	private int[] elems;
-	private int[] edges;
-	private int height;
-	private int breadth;
+	private int width;
 	private int size;
 	
     public AngryChildren(int n, int k, int[] packets) {
     	this.elems=packets;
-    	this.height=n;
-    	this.breadth=2;
-    	this.size=k;  //max size of a valid set
-    	edges=new int[height];
-    	for(int i=0;i<height;i++)	edges[i]=0;
-    	
+    	this.size=n;
+    	this.width=k;
 	}
-    
-    public Integer peek(int level){
-    	if(edges[level] == breadth){
-    		edges[level]=0;
-    		return null;
-    	}
-    	Integer result=edges[level];
-    	edges[level]+=1;
-    	return result;
-    }
-    
+   
     public int solve() {
-    	Integer elem;
-    	int level=0;
-    	Deque<Integer> closed=new ArrayDeque<Integer>();
-    	List<Integer> unfairnessList=new ArrayList<Integer>();
-    	int closedSize=0;
-    	while(level>=0){
-    		if((elem=peek(level))==null){
-    			level--;
-    			elem=closed.pollFirst();
-    			if(elem!=null)
-    				closedSize-= elem==1 ? 1 : 0;
-    			continue;
-    		}
-    		level++;
-    		closed.push(elem);
-    		if(elem==1)
-    			closedSize++;
-    		if(closedSize==size){
-    			unfairnessList.add(unfairness(closed));
-    			level--;
-    			elem=closed.pollFirst();
-    			if(elem!=null)
-    				closedSize-= elem==1 ? 1 : 0;
-    			continue;
-    		}
-    		if(level==height){
-    			level--;
-    			elem=closed.pollFirst();
-    			if(elem!=null)
-    				closedSize-= elem==1 ? 1 : 0;
-    			continue;
-    		}
-    	}
-    	int min=unfairnessList.get(0);
-    	int val;
-    	for(int i=1;i<unfairnessList.size();i++){
-    		val=unfairnessList.get(i);
-    		if(min>val)
-    			min=val;
+    	int rightmost,unfairness,min=-1;
+    	for(int leftmost=0;leftmost<size;leftmost++){
+    		rightmost=width+leftmost-1;
+    		if(rightmost>=size)		break;
+    		unfairness=elems[rightmost]-elems[leftmost];
+    		if(unfairness<min || min==-1)
+    			min=unfairness;
     	}
     	return min;
-	}
-
-	private Integer unfairness(Deque<Integer> closed) {
-		Iterator<Integer> iter=closed.iterator();
-		int level=-1;
-		int min=-1;
-		int max=0;
-		while(iter.hasNext()){
-			Integer val=iter.next();
-			level++;
-			if(val==1){
-				if(min==-1)
-					min=elems[level];
-				max=elems[level];
-			}
-		}
-		return max-min;
 	}
 
 	public static void main(String[] args) {
