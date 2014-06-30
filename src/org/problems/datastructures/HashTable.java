8097e3d6f65d74922cd4f0cdcc11977b066a2544
Victor Regalado <vrrs0988@gmail.com>
2014-06-30 01:12:54 -0400

    Added duplicate finder problem solution

---
+++ src/org/problems/datastructures/HashTable.java
@@ -0,0 +1,9 @@
+package org.problems.datastructures;
+/**
+ * Problem: implement using collision resolution by quadratic probing a hash table. 
+ * @author victor regalado
+ *
+ */
+public class HashTable {
+
+}
