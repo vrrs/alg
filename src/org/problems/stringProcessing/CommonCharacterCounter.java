3c0bdb4a45b9b9ccf28fe3f5655df9ed6fe87d7c
Victor Regalado <vrrs0988@gmail.com>
2014-07-26 22:46:16 -0400

    Update CommonCharacterCounter.java

--- src/org/problems/stringProcessing/CommonCharacterCounter.java
+++ src/org/problems/stringProcessing/CommonCharacterCounter.java
@@ -1,8 +1,6 @@
 package org.problems.stringProcessing;
-
+import java.util.HashSet;
 import java.util.Set;
-
-import com.google.common.collect.Sets;
 
 /**
  * 	Problem: Given String[], find the # of common characters. 
@@ -12,20 +10,38 @@ import com.google.common.collect.Sets;
 public class CommonCharacterCounter {
 	
 	public int getTotalOfCommonCharacters(String[] words){
-		Set<Character> intersection = Sets.newHashSet();
-		Set<Character> wordSet = Sets.newHashSet();
-		insertFirstWord(words[0], intersection);
-		for(int i = 1; i < words.length; i++){
+		if(words.length == 1)	return words[0].length();
+		Set<Character> intersection = intersectionOf(words[0].toCharArray(), words[1].toCharArray());
+		for(int i = 2; i < words.length; i++){
 			String word = words[i];
-			wordSet.clear();
-			for(Character c : word.toCharArray())	wordSet.add(c);
-			intersection.retainAll(wordSet);
+			intersection = intersectionOf(intersection ,word.toCharArray());
 		}
 		return intersection.size();
 	}
 
-	private void insertFirstWord(String firstWord, Set<Character> intersection) {
-		for(Character c : firstWord.toCharArray())
-			intersection.add(c);
+	private Set<Character> intersectionOf(char[] firstWordChars, char[] secondWordChars) {
+		Set<Character> intersection = new HashSet<Character>();
+		for(Character c : firstWordChars)	intersection.add(c);
+		return intersectionOf(intersection, secondWordChars);
+	}
+
+	private Set<Character> intersectionOf(Set<Character> intersection, char[] secondWordChars) {
+		char mark = getMark(secondWordChars);
+		for(int i = 0; i <  secondWordChars.length; i++)
+			if(!intersection.contains(secondWordChars[i]))
+				secondWordChars[i] = mark;
+		intersection.clear();
+		for(char c : secondWordChars)
+			if(c != mark)
+				intersection.add(c);
+		return intersection;
+	}
+
+	private char getMark(char[] chars){
+		char max = chars[0];
+		for(int i = 1; i< chars.length; i++)
+			if(max < chars[i])
+				max = chars[i];
+		return (char) ((int) max + 1);
 	}
 }
