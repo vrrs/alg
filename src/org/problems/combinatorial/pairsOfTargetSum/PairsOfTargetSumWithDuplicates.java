efb1f680d0efbbd969bc4f501a722a28f2fa8d48
Victor Regalado <vrrs@users.noreply.github.com>
2014-08-13 23:47:41 -0400

    Formatted it

--- src/org/problems/combinatorial/pairsOfTargetSum/PairsOfTargetSumWithDuplicates.java
+++ src/org/problems/combinatorial/pairsOfTargetSum/PairsOfTargetSumWithDuplicates.java
@@ -9,20 +9,20 @@ import java.util.HashMap;
 
 final class PairsOfTargetSumWithDuplicates implements PairsOfTargetSum{
 
-	public void printPairs(int ar[], int target){
-		  Map<Integer, Integer> hist = histogramOf(ar);
-		  for(Integer elem1 : hist.keySet()){
-			  int count1 = hist.get(elem1);
-			  if(count1 <= 0 )	continue;
-			  int elem2 = target - elem1;
-			  int count = Math.min(count1, hist.get(elem2));
-			  count = elem2 == elem1	?	count / 2	:	count;
-			  for(int i = 0; i < count; i++)
-				  System.out.print("("+elem1+","+elem2+"),");
-			  hist.put(elem2, 0);
-			  hist.put(elem1, 0);
-		  }
-	}
+  public void printPairs(int ar[], int target){
+    Map<Integer, Integer> hist = histogramOf(ar);
+    for(Integer elem1 : hist.keySet()){
+	int count1 = hist.get(elem1);
+	if(count1 <= 0 )	continue;
+	int elem2 = target - elem1;
+	int count = Math.min(count1, hist.get(elem2));
+	count = elem2 == elem1	?	count / 2	:	count;
+	for(int i = 0; i < count; i++)
+		System.out.print("("+elem1+","+elem2+"),");
+	hist.put(elem2, 0);
+	hist.put(elem1, 0);
+    }
+ }
 
 
   private Map<Integer, Integer> histogramOf(int[] ar){
