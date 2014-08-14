883cf0418986b184d1398ec58a6137f1ddfd986e
Victor Regalado <vrrs@users.noreply.github.com>
2014-08-13 23:43:36 -0400

    Update PairsOfTargetSums.java

--- src/org/problems/combinatorial/pairsOfTargetSum/PairsOfTargetSums.java
+++ src/org/problems/combinatorial/pairsOfTargetSum/PairsOfTargetSums.java
@@ -1,7 +1,13 @@
+package org.problems.combinatorial.pairsOfTargetSum;
+
 public final PairsOfTargetSums{
   private PairsOfTargetSums(){}
   
   public PairsOfTargetSum newPrinterWithSupportForDuplicates(){
-    return new 
+    return new PairsOfTargetSumWithDuplicates();
+  }
+  
+   public PairsOfTargetSum newPrinter(){
+    return new PairsOfTargetSumWithUniqueElements();
   }
 }
