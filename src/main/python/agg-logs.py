ae482157cfc3a83e7dda0e75386d03a3fbca6ce5
vregalado <vregalado@localhost>
2018-04-28 13:09:10 -0400

    ALG-003 Updated agg-logs so it skips bad lines

--- src/main/python/agg-logs.py
+++ src/main/python/agg-logs.py
@@ -7,6 +7,7 @@
 
 import os
 import argparse
+import datetime
 
 def to_file(csv, name):
     file = open(name, 'w+')
@@ -18,15 +19,47 @@ def to_file(csv, name):
             file.write(date_until_min + "," 
                        + str(csv[status_code][date_until_min]) + "\n") 
     file.close()
+    
+def get_valid_http_status_code(token):
+    status_code = int(token)
+    if status_code < 600 and status_code > 99:
+        return str(status_code)
+    else:
+        return None
+
+def try_parse_pos_int(val):
+    try :
+        val = int(val)
+        return val
+    except:
+        return -1
+
+def is_valid_date(token):
+    try:
+        datetime.datetime.strptime(token, '%d/%b/%Y')
+        return True
+    except:
+        return False
+
+def get_valid_date_until_min(token):
+    parts = token.split(":")
+    if len(parts) == 3 and is_valid_date(parts[0]): 
+        hour = try_parse_pos_int(parts[1])
+        min = try_parse_pos_int(parts[2])
+        if hour >= 0 and hour <= 24 and min >= 0 and min <= 59:
+            return token 
+    return None
 
 def get_date_and_status_code(logline):
     date_until_min = None
     status_code = None
     for token in logline.split(" "):
+        if len(token) == 0:
+            continue
         if token[0] == "[":
-            date_until_min = token[1:-3]
+            date_until_min = get_valid_date_until_min(token[1:-3])
         if token.isdigit():
-            status_code = token
+            status_code = get_valid_http_status_code(token)
         if not date_until_min is None and not status_code is None:
             break
     return date_until_min, status_code
@@ -40,9 +73,14 @@ def main(dir, name):
     csv = dict()
     for filename in list_files(dir):
         print "Processing " + filename
+        skipped_line_count = total_line_count = 0
         with open(filename, 'r') as logfile:
             for logline in logfile:
+                total_line_count = total_line_count + 1
                 date_until_min, status_code = get_date_and_status_code(logline)
+                if date_until_min is None or status_code is None :
+                    skipped_line_count = skipped_line_count + 1
+                    continue
                 if not status_code in csv:
                     csv[status_code] = dict()
                 if date_until_min in csv[status_code]:
@@ -50,7 +88,9 @@ def main(dir, name):
                     csv[status_code][date_until_min] = cur_count + 1
                 else :
                     csv[status_code][date_until_min] = 1
-        print "Finished processing " + filename
+        pct_skipped = int(100*(skipped_line_count / float(total_line_count)))
+        msg = " ".join(["Finished processing", filename, ". Percentage of lines skipped:", str(pct_skipped)])
+        print msg
     to_file(csv, name)
 
 def get_args():
