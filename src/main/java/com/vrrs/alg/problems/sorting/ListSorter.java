a78c71ca5e656af28ad1d54a8ca987ec930239f5
victor <victor.regalado@tumblr.com>
2018-11-23 18:01:27 -0500

    Added k list sort problem solution

---
+++ src/main/java/com/vrrs/alg/problems/sorting/ListSorter.java
@@ -0,0 +1,38 @@
+
+package com.vrrs.alg.problems.sorting;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListSorter {
+
+  public List<Integer> sortKList(List<List<Integer>> lists){
+    return sortKList(0, lists.size() - 1, lists);
+  }
+
+  private List<Integer> sortKList(int lb, int ub, List<List<Integer>> lists) {
+    if (lb > ub) {
+      return lists.get(lb);
+    }
+    int mid = (lb + ub) / 2;
+    List<Integer> left = sortKList(lb, mid, lists);
+    List<Integer> right = sortKList(mid + 1, ub, lists);
+    return merge(left, right);
+  }
+
+  private List<Integer> merge(List<Integer> left, List<Integer> right) {
+    List<Integer> result = new ArrayList<>();
+    int i = 0, j = 0;
+    for(;i < left.size() && j < right.size();){
+      if(left.get(i) <= right.get(j)){
+        result.add(left.get(i++));
+      } else {
+        result.add(right.get(j++));
+      }
+    }
+    for(int k = i; k < left.size(); k++)  result.add(left.get(k));
+    for(int k = j; k < right.size(); k++)  result.add(right.get(k));
+    return result;
+  }
+
+}
---
+++ src/test/java/com/vrrs/alg/problems/sorting/TestListSorter.java
@@ -0,0 +1,20 @@
+package com.vrrs.alg.problems.sorting;
+
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+public class TestListSorter {
+
+	@Test
+	public void willSortAllLists() {
+    List<Integer> aList = ImmutableList.of(1,4,5);
+    List<Integer> anotherList = ImmutableList.of(2,8,10,11);
+		List<List<Integer>> lists = ImmutableList.of(aList, anotherList);
+
+		assertThat(new ListSorter().sortKList(lists))
+      .contains(ImmutableList.of(1, 2, 4, 5, 8, 10, 11));
+	}
+}
